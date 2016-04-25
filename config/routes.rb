7c00fa5bb5905906af9a9a78e51dc786dc9c60d5
Joshua Shepherd <never.noes.best@gmail.com>
2016-04-25 15:13:38 -0500

    Made changes to views/added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'pages/contact'
+
+  get 'pages/about'
+
   root to: 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
